08077877291169129c705c6f3367df3b8b1e294b
Kira Droganova <kira.droganova@gmail.com>
2019-04-17 23:12:14 +0200

    add 4 more frameworks

--- json-to-tikz.py
+++ json-to-tikz.py
@@ -4,7 +4,7 @@
 MRP-json to tikz-dependency converter.
 
 Usage:
-  $ ./json-to-tikz.py -i json_data
+  $ ./json_tikz.py -i json_data
 
 """
 
@@ -18,6 +18,11 @@ def parse_arguments():
                         type=str, action='store',
                         default=None, dest='input_file_name',
                         help='input file name', required=True)
+    parser.add_argument('-f', '--framework', metavar='F',
+                        type=str, action='store',
+                        default=None, dest='framework',
+                        choices=['psd', 'dm', 'ucca', 'amr', 'eds'],
+                        help='framework', required=True)
 
     args = parser.parse_args()
     return args
@@ -28,42 +33,73 @@ def process(args):
           '\\usepackage{tikz-dependency}\n'
           '\centering\n'
           '\\begin{document}')
-    for line in open(args.input_file_name, 'r'):
-        json_line = json.loads(line)
-        text = json_line['input']
-        nodes = json_line['nodes']
-        edges = json_line['edges']
-        generate_latex(nodes, edges, text)
+    with open(args.input_file_name, 'r') as i_file:
+        for line in i_file:
+            json_line = json.loads(line)
+            text = json_line['input']
+            nodes = json_line['nodes']
+            edges = json_line['edges']
+            generate_latex(nodes, edges, text, args.framework)
     print('\end{document}')
 
-def generate_latex(nodes, edges, text):
+def generate_latex(nodes, edges, text, framework):
 
     print('\\resizebox{\columnwidth}{!}{%\n'
           '\\begin{dependency}[theme = default]\n'
           '\\begin{deptext}[column sep=.7em]')
-    id_to_index = process_nodes(nodes)
+    id_to_index = process_nodes(nodes, framework, text)
     print('\end{deptext}')
     process_edges(edges, id_to_index)
     print('\end{dependency}%\n}\n')
     print('\n' + text + '\n')
 
-def process_nodes(nodes):
-    node_labels = [node['label'].replace('#', '\#') for node in nodes]
+def process_nodes(nodes, framework, text):
+
+    if framework == 'ucca':
+        node_labels = []
+        for node in nodes:
+            if 'anchors' in node:
+                node_text = []
+                for anchor in node['anchors']:
+                    node_text_part = text[anchor['from']:anchor['to']]
+                    node_text.append(node_text_part)
+                node_text = ' '.join(node_text)
+                node_labels.append(node_text)
+            else:
+                node_labels.append('EMPTY')
+
+    elif framework == 'eds':
+        node_labels = []
+        node_wordforms = []
+        for node in nodes:
+            node_labels.append(node['label'].replace('_', '\_'))
+            if 'values' in node:
+                node_wordforms.extend(node['values'])
+            else:
+                node_wordforms.append(' ')
+    else:
+        node_labels = [node['label'].replace('#', '\#') for node in nodes]
+
     node_labels_for_latex = ' \& '.join(node_labels)
+    print(node_labels_for_latex + ' \\\\')
+
+    if framework == 'eds':
+        node_wordforms_for_latex = ' \& '.join(node_wordforms)
+        print(node_wordforms_for_latex + '\\\\')
+
     id_to_index = {}
     for i, node in enumerate(nodes):
         id_to_index[node['id']] = str(i + 1)
-    
-    print(node_labels_for_latex + ' \\\\')
+
     return id_to_index
 
 def process_edges(edges, id_to_index):
     for edge in edges:
-        edge_for_latex = ('\depedge{' + id_to_index[edge['source']] + 
-                          '}{' + id_to_index[edge['target']] + 
+        edge_for_latex = ('\depedge{' + id_to_index[edge['source']] +
+                          '}{' + id_to_index[edge['target']] +
                           '}{' + edge['label'] + '}')
         print(edge_for_latex)
-        
+
 if __name__ == '__main__':
     args = parse_arguments()
     process(args)
