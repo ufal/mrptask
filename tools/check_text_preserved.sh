e06530576eb8c37311d5ac9128d0603c96c66dd7
Dan Zeman <zeman@ufal.mff.cuni.cz>
2019-05-22 11:27:34 +0200

    A script that should find the encoding problems Kira had on cluster.

---
+++ tools/check_text_preserved.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+MRP=/net/work/people/zeman/mrptask
+UD24=/net/data/universal-dependencies/2.4
+EUD=/lnet/spec/work/people/droganova/Data_for_Enhancer/final_2.4
+UDTOOLS=/net/work/people/zeman/unidep/tools
+cd $EUD
+for i in UD_* ; do
+  echo $i
+  for j in $i/*.conllu ; do
+    echo "  $j"
+    $TOOLS/conllu_to_text.pl $j > /tmp/ctt.txt
+    diff $UD24/`basename $j .conllu`.txt /tmp/ctt.txt
+    rm /tmp/ctt.txt
+  done
+done
