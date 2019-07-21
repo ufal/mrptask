bc15576564cf240263a657704e39e54212c9b97e
Dan Zeman <zeman@ufal.mff.cuni.cz>
2019-07-21 23:05:56 +0200

    Just one log per treebank.

--- tools/add_pas_to_all.sh
+++ tools/add_pas_to_all.sh
@@ -4,11 +4,14 @@ MRPTASK=/net/work/people/zeman/mrptask
 cd $MRPTASK/deepud/data
 mkdir -p deep
 for i in enhanced/UD_* ; do
+  echo $i
   ibase=`basename $i`
   mkdir -p deep/$ibase
+  cat $i/*.conllu > deep/$ibase/all.conllu
+  ../../tools/add_pas.pl --udpath deep --release http://hdl.handle.net/11234/1-2988 --folder $ibase --file all.conllu > /dev/null 2> deep/$ibase/all.log
   for j in $i/*.conllu ; do
     echo $j
     jbase=`basename $j .conllu`
-    ../../tools/add_pas.pl --udpath enhanced --release http://hdl.handle.net/11234/1-2988 --folder $ibase --file $jbase.conllu > deep/$ibase/$jbase.conllup 2> deep/$ibase/$jbase.log
+    ../../tools/add_pas.pl --udpath enhanced --release http://hdl.handle.net/11234/1-2988 --folder $ibase --file $jbase.conllu > deep/$ibase/$jbase.conllup 2> /dev/null
   done
 done
